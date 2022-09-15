bee3b0a50a4a793d0a8e9fbe4d9db7bed4c84763
leoarthurwall <leowall93@gmail.com>
2022-09-15 19:28:50 +0100

    added plus and minus 5 buttons, multiply and divde by 2 buttons aswell to make functionality more dynamic

--- src/App.js
+++ src/App.js
@@ -5,20 +5,36 @@ function App() {
   //state that stores count value
   const[count, setCount] = useState(0)
 
-  //function that fires when increment button is clicked
-  const increment = () => {
+  //function that fires when buttons are clicked
+  const incrementOne = () => {
      setCount (count + 1)
   }
-  //function that fires when increment button is clicked
-  const decrement = () => {
+  const decrementOne = () => {
     setCount (count - 1)
  }
+ const incrementFive = () => {
+  setCount (count + 5)
+}
+const decrementFive = () => {
+ setCount (count - 5)
+}
+const multiplyTwo = () => {
+  setCount ( count * 2)
+}
+const divideTwo = () => {
+  setCount (count / 2)
+}
 
   return (
     <div className="App">
       <h1>Counter</h1>
-      <button className='increment' onClick={increment}>+</button>
-      <button className='decrement' onClick={decrement} >-</button>
+      <button  onClick={incrementOne}>+ 1</button>
+      <button  onClick={decrementOne} >- 1</button>
+      <button  onClick={incrementFive} >+ 5</button>
+      <button  onClick={decrementFive} >- 5</button>
+      <button  onClick={multiplyTwo} >x 2</button>
+      <button  onClick={divideTwo} >÷ 2</button>
+
       <h2>{count}</h2>
     </div>
   );
