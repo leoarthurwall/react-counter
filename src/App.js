2333210d3a7b3bbfdc64382ce6b914b584991aa5
leoarthurwall <leowall93@gmail.com>
2022-09-15 16:01:32 +0100

    added input that console logs the event.target.value of the inputted text

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ function App() {
   function handleClick () {
     console.log("button clicked")
   }
+  function handleChange (event) {
+    console.log(event.target.value)
+  }
   return (
     <div className="App">
       <h1>Hello World</h1>
       <button onClick={handleClick}>Click me</button>
+      <input onChange={handleChange} placeholder="write something..."></input>
     </div>
   );
 }
