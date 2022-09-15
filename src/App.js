990bb5b02fbb1323dbc65f8dc026ac4ff46bbd0a
leoarthurwall <leowall93@gmail.com>
2022-09-15 15:57:02 +0100

    added a handle click that displays console log when button is clicked

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 
 function App() {
+  
+  function handleClick () {
+    console.log("button clicked")
+  }
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
