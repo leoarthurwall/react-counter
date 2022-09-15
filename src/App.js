9088b3f57a06dc5b97ff307c62a956a56b539635
leoarthurwall <leowall93@gmail.com>
2022-09-15 19:19:58 +0100

    created basic counter using state, functional components,and  onClick event handlers

--- src/App.js
+++ src/App.js
@@ -2,25 +2,24 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  //state that stores count value
+  const[count, setCount] = useState(0)
 
-  const [output, setOutput] = useState('')
-  const [submit, setSubmit] = useState(false)
-  
-  function handleClick () {
-    console.log("button clicked")
-    setSubmit(!submit)
-    console.log(submit)
+  //function that fires when increment button is clicked
+  const increment = () => {
+     setCount (count + 1)
   }
-  function handleChange (event) {
-    console.log(event.target.value)
-    setOutput(event.target.value)
-  }
+  //function that fires when increment button is clicked
+  const decrement = () => {
+    setCount (count - 1)
+ }
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <input onChange={handleChange} placeholder="write something..."></input>
-     <button onClick={handleClick}>submit</button>
-      <h2>{output}</h2>
+      <h1>Counter</h1>
+      <button className='increment' onClick={increment}>+</button>
+      <button className='decrement' onClick={decrement} >-</button>
+      <h2>{count}</h2>
     </div>
   );
 
