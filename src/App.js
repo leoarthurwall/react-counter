e723df50cfe72c3acaffdf6cb8750e5933e7e5bb
leoarthurwall <leowall93@gmail.com>
2022-09-15 16:13:46 +0100

    added state for the submit button, set to false which changes to the opposite state when clicked

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [output, setOutput] = useState('')
+  const [submit, setSubmit] = useState(false)
   
   function handleClick () {
     console.log("button clicked")
+    setSubmit(!submit)
+    console.log(submit)
   }
   function handleChange (event) {
     console.log(event.target.value)
+    setOutput(event.target.value)
   }
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <button onClick={handleClick}>Click me</button>
       <input onChange={handleChange} placeholder="write something..."></input>
+     <button onClick={handleClick}>submit</button>
+      <h2>{output}</h2>
     </div>
   );
+
 }
 
 export default App;
